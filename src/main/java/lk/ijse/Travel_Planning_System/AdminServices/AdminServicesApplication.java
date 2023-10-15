6924a10c4dbcf4b958f6ea23e5ca8fc373c2f265
KasunUmesh <kasunumesh38@gmail.com>
2023-10-16 00:51:23 +0530

    DTO and Entity Conversion class Create

--- src/main/java/lk/ijse/Travel_Planning_System/AdminServices/AdminServicesApplication.java
+++ src/main/java/lk/ijse/Travel_Planning_System/AdminServices/AdminServicesApplication.java
@@ -1,10 +1,17 @@
 package lk.ijse.Travel_Planning_System.AdminServices;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AdminServicesApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminServicesApplication.class, args);
