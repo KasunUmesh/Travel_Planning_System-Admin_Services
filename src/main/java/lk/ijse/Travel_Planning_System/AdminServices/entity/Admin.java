bb57b4454e89abc876dc01ddddd2b6b7a113cc8b
KasunUmesh <kasunumesh38@gmail.com>
2023-10-15 21:14:05 +0530

    Admin Entity Created

---
+++ src/main/java/lk/ijse/Travel_Planning_System/AdminServices/entity/Admin.java
@@ -0,0 +1,23 @@
+package lk.ijse.Travel_Planning_System.AdminServices.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Admin implements SuperEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String adminID;
+    @Column(nullable = false)
+    private String adminName;
+    @Column(nullable = false)
+    private String adminPassword;
+    @Lob
+    @Column(columnDefinition = "Text")
+    private String adminIMG;
+}
