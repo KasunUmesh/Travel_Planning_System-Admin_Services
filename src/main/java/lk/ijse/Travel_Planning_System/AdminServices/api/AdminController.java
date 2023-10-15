169ee369355d4a387127115ffcff53528648e72f
KasunUmesh <kasunumesh38@gmail.com>
2023-10-16 01:44:12 +0530

    Admin save function created

--- src/main/java/lk/ijse/Travel_Planning_System/AdminServices/api/AdminController.java
+++ src/main/java/lk/ijse/Travel_Planning_System/AdminServices/api/AdminController.java
@@ -1,13 +1,25 @@
 package lk.ijse.Travel_Planning_System.AdminServices.api;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.Travel_Planning_System.AdminServices.dto.AdminDTO;
+import lk.ijse.Travel_Planning_System.AdminServices.service.AdminService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/admin")
 @CrossOrigin("*")
 public class AdminController {
 
+    private final AdminService adminService;
 
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    AdminDTO saveAdmin(@RequestBody AdminDTO adminDTO) {
+        System.out.println(adminDTO);
+        return adminService.saveAdmin(adminDTO);
+    }
 }
