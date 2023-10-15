cd50be5bb0cafd2ee6ebdd336fab3c4895fc5e03
KasunUmesh <kasunumesh38@gmail.com>
2023-10-15 18:07:14 +0530

    Init Project

---
+++ src/main/java/lk/ijse/Travel_Planning_System/AdminServices/AdminServicesApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.Travel_Planning_System.AdminServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServicesApplication.class, args);
+	}
+
+}
